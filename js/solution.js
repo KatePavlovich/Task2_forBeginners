c0ccefcb5584b7399d20b9b1c77794a32e34d361
KatePavlovich <asylumhere@mail.ru>
2018-03-03 14:11:15 +0300

    solution commit

--- js/solution.js
+++ js/solution.js
@@ -10,9 +10,31 @@
      * @returns {number} кол-во островов
      */
     function solution(map) {
-        // todo: подсчитать кол-во островов на карте
-        return 0;
-    }
+
+        let island = 0;
+        let len=map.length;
+        for (let i=0; i<len; i++) {
+        let x=map[i].length;
+            for (let j=0; j<x;j++) {
+                if(map[i][j] === 1){
+                    island++;
+                    helper(i, j);
+                }
+            }
+        }
+        
+        function helper (i,j) {
+          if (map[i] && map[i][j]) {
+            map[i][j] = 0;
+              
+              helper(i-1, j);
+              helper(i, j-1);
+              helper(i, j+1);
+              helper(i+1, j);
+            }
+          }
+  return island;        
+}
 
     root.SHRI_ISLANDS.solution = solution;
 })(this);
